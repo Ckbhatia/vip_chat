8dfdae0cb1cf1695c2faef3777236a62ea6dc610
Ckbhatia <chetansain86@gmail.com>
2022-02-06 00:01:06 +0530

    feat: add configuration file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  baseURL: process.env.REACT_APP_BASE_URL,
+  accountSid: process.env.REACT_APP_TWILIO_ACCOUNT_SID,
+  authTokenUrl: process.env.REACT_APP_TWILIO_AUTH_TOKEN_URL,
+  tempToken: process.env.REACT_APP_TEMP_TOKEN
+}
+
+export default config;
