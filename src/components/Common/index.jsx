63262f80f1c386fea72671d9e96b75e5950b0f4b
Ckbhatia <chetansain86@gmail.com>
2022-02-06 18:06:13 +0530

    feat: create a common button

---
+++ src/components/Common/index.jsx
@@ -0,0 +1,10 @@
+import tw from "twin.macro";
+
+export const StyledButton = tw.button`
+  px-4 py-2
+  border-2 border-blue-200 rounded-md
+  bg-blue-200
+  text-black
+  ml-2
+  hover:bg-blue-300 border-blue-200 
+`;
