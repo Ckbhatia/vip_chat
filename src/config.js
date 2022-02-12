0849cc5312758f7f9781f300e8ef3ec2fea3f027
Ckbhatia <chetansain86@gmail.com>
2022-02-12 20:36:57 +0530

    feat: add variables

--- src/config.js
+++ src/config.js
@@ -2,7 +2,10 @@ const config = {
   baseURL: process.env.REACT_APP_BASE_URL,
   accountSid: process.env.REACT_APP_TWILIO_ACCOUNT_SID,
   authTokenUrl: process.env.REACT_APP_TWILIO_AUTH_TOKEN_URL,
-  tempToken: process.env.REACT_APP_TEMP_TOKEN
+  tempToken: process.env.REACT_APP_TEMP_TOKEN,
+  userOne: process.env.REACT_APP_USER_ONE,
+  userTwo: process.env.REACT_APP_USER_TWO,
+  uniqueChatName: process.env.REACT_APP_UNIQUE_CHAT_NAME,
 }
 
 export default config;
