24c8e1d5c0bc33c138aab2e94087f327ccdf7fbc
Ckbhatia <chetansain86@gmail.com>
2022-04-05 23:25:58 +0530

    feat: add unfurl & pass url

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,12 +1,17 @@
+import Unfurl from "../Unfurl";
 import { StyledMessageContainer, StyledText } from "./Styles";
 
-const Message = ({text, message, currentUser}) => {
+const Message = ({ text, message, currentUser }) => {
   const isCurrentUser = message?.state?.author === currentUser;
+
+  const isMusicLink = (text) => {
+    return text?.includes("https://music.apple.com");
+  };
   return (
     <StyledMessageContainer isCurrentUser={isCurrentUser}>
-      <StyledText>{text}</StyledText>
+      {isMusicLink(text) ? <Unfurl url={text} /> : <StyledText>{text}</StyledText>}
     </StyledMessageContainer>
-  )
-}
+  );
+};
 
 export default Message;
