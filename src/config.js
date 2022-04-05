1396904062f95d068ec8c35e4ba9cdfc56041777
Ckbhatia <chetansain86@gmail.com>
2022-04-05 23:26:13 +0530

    feat: create searchUrl

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const config = {
   baseURL: process.env.REACT_APP_BASE_URL,
   accountSid: process.env.REACT_APP_TWILIO_ACCOUNT_SID,
   authTokenUrl: process.env.REACT_APP_TWILIO_AUTH_TOKEN_URL,
+  searchUrl: process.env.REACT_APP_SEARCH_URL,
   tempToken: process.env.REACT_APP_TEMP_TOKEN,
   userOne: process.env.REACT_APP_USER_ONE,
   userTwo: process.env.REACT_APP_USER_TWO,
